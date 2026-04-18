bfae855243622ad329d32fb06954bf35c03079d5
Moutite <stephaniebuisson1115@gmail.com>
2026-04-18 09:44:32 +0200

    Hardcode Stripe public key

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,13 +1,12 @@
 import { loadStripe, Stripe } from '@stripe/stripe-js';
 
-const STRIPE_KEY = import.meta.env.VITE_STRIPE_PUBLIC_KEY;
-console.log('DEBUG STRIPE: Public Key starts with:', STRIPE_KEY?.slice(0, 12));
+console.log('DEBUG STRIPE: Public Key starts with:', 'pk_live_51STHh');
 
 let stripePromise: Promise<Stripe | null>;
 
 export const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(STRIPE_KEY);
+    stripePromise = loadStripe('pk_live_51STHhnREq6ZFjXVXCHlIWoqNkdEfILxmuyN22g8alvqLuJB02rOefwMdwUTBkg5nRs76NzclHiuHiMfJqLFllRBn00aVLN5FOO');
   }
   return stripePromise;
 };
